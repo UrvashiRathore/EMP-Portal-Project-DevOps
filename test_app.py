d20cd64dd22a939b318abc47b2f0059f3f7b34c5
UrvashiRathore <89251583+UrvashiRathore@users.noreply.github.com>
2023-06-27 00:13:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,48 +5,35 @@ from app import app, db
 
 @pytest.fixture
 def client():
-    app_context = app.app_context()
-    app_context.push()
-    initDB()
-    yield app.test_client()
-    truncateDB()
-    app_context.pop()
-
-
-def initDB():
-    DATABASE = 'test_emp_db.db'
-    app.config.update(
-        SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE
-    )
-
-
-def truncateDB():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     with app.app_context():
-        models.Employee.query.delete()
-        db.session.commit()
+        db.create_all()
+        yield app.test_client()
+        db.drop_all()
 
 
-def test_index():
-    client = app.test_client()
+def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
 
-
 def test_add(client):
-    test_data = {'name': 'Mickey Test',
-                 'gender': 'male',
-                 'address': 'IN',
-                 'phone': '0123456789',
-                 'salary': '2000',
-                 'department': 'Sales'}
-    client.post('/add', data=test_data)
+    test_data = {
+        'name': 'Mickey Test',
+        'gender': 'male',
+        'address': 'IN',
+        'phone': '0123456789',
+        'salary': '2000',
+        'department': 'Sales'
+    }
+    response = client.post('/add', data=test_data)
+    assert response.status_code == 200
     with app.app_context():
         assert models.Employee.query.count() == 1
 
 
-def test_edit():
-    client = app.test_client()
+def test_edit(client):
     response = client.post('/edit/0')
     assert response.status_code == 200
     assert b"Sorry, the employee does not exist." in response.data
